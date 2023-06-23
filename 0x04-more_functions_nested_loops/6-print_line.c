5bf0f196ae7b512be63bf803c4d0689a67d84de3
amgd-thabit <amgadthabit9@gmail.com>
2023-06-23 16:53:01 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the - character
+ *	should be printed
+*/
+void print_line(int n)
+{
+	int Inchr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (Inchr = 1; Inchr <= n; Inchr++)
+			_putchar('.');
+		_putchar('\n');
+	}
+}
