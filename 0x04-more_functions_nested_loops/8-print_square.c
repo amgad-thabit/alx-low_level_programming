ded95446c8f79c005fba1cf8b0f9174181e90c17
amgd-thabit <amgadthabit9@gmail.com>
2023-06-23 17:27:03 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,8 @@
  * print_square - print a square using the character #
  *
  * @size: is the size of the square
- *	Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
 */
 void print_square(int size)
 {
