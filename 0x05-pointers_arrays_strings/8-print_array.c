a247b4776ccba9463005d9b3ea1214a621331df2
amgd-thabit <amgadthabit9@gmail.com>
2023-06-26 17:53:21 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers.
+ * @n: elenents paraneter input
+ * @a: string paraneter input
+ * Return: Nothing
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	print£ ("\n");
+}
