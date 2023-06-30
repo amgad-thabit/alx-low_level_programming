3ebad74a78cf476ca3e2fa51b3c35de34eab63d5
amgd-thabit <amgadthabit9@gmail.com>
2023-06-30 14:31:06 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * putchas- waltes the charactar a to stdout
+ * @c: The character to print
+ *
+ * Raturn: On success 1.
+ * On errors, -1 is returned, and erro is set appropriately.
+*/
+int _putchar (char c)
+{
+        return (write (1, &c, 1));
+}
