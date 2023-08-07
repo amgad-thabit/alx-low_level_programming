49073c59b5047555927a943ab2861716d3253152
amgd-thabit <amgadthabit9@gmail.com>
2023-08-07 06:39:33 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 	file = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (file == -1)
 		return (0);
 	bytes = read(file, &buf[0], bytes);
 	close(file);
