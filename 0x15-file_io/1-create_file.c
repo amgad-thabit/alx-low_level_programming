a41dc0c037dc8ce4a2286182c2153f2cd8ecaf64
amgd-thabit <amgadthabit9@gmail.com>
2023-08-07 08:16:35 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	name = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	name = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (name == -1)
 		return (-1);
 	if (len)
