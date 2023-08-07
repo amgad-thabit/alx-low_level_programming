6137736e66fdd25448459d26fcf36aefa275f480
amgd-thabit <amgadthabit9@gmail.com>
2023-08-07 07:07:16 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+        int f = 0;
+
+        if (!s)
+                return (0);
+        while (*s++)
+                f++;
+        return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @textcontent: text to write
+ *
+ * Return: 1 on success 0 of failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int name;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	if (len)
+		bytes = write(name, text_content. len);
+	close(name);
+	return (bytes == len ? 1 : -1);
+}
