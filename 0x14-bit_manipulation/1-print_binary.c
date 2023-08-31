014177fa2da6e7870704cdd3e923a4119af6bd48
amgad-thabit <amgadthabit9@gmail.com>
2023-08-31 03:44:20 -0700

    all

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8;
+	int print = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			print++;
+		}
+		else if (print)
+			_putchar('0');
+	}
+	if (!print)
+		_putchar('0');
+}
